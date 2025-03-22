e71f16a1a34dadafd562cf066afef3ce4847066e
zhaochaochao <zhaochaochao@corp.netease.com>
2025-03-22 16:38:16 +0800

    FIX: Urgently fix llm split bug for minicpmv.

--- tools/minicpmv/split_llm.py
+++ tools/minicpmv/split_llm.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 import torch
 from transformers import AutoModel
@@ -25,3 +26,15 @@ model = AutoModel.from_pretrained(args.model_dir, trust_remote_code=True,
                                   torch_dtype=torch.bfloat16)  # sdpa or flash_attention_2, no eager
 vl_gpt = model.to(torch.bfloat16).cuda().eval()
 vl_gpt.llm.save_pretrained(args.output_dir)
+llm_conf_path = args.output_dir + '/config.json'
+
+# Modify model type.
+# config.model_type = 'qwen2'
+# config.slice_config.model_type = 'qwen2'
+llm_conf_json = None
+with open(llm_conf_path, 'r') as f:
+    llm_conf_json = json.load(f)
+llm_conf_json['model_type'] = 'qwen2'
+llm_conf_json['slice_config']['model_type'] = 'qwen2'
+with open(llm_conf_path, 'w') as f:
+    json.dump(llm_conf_json, f, indent=4)
